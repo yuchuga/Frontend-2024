246f91f084dda6873de7f1aa2fd9bc228f3e5f09
yuchuga <yuan_chungschumi@hotmail.com>
2024-03-21 18:26:49 +0800

    update test file

--- src/test.js
+++ src/test.js
@@ -1,51 +1,11 @@
-const person = {
-  name: 'John',
-  age: 30,
-  location: 'Dubai'
-};
-
-const num = [11, 30, 22, 9, 19]
-const sum = num.reduce((acc, curr) => acc + curr)
-// console.log(sum)
-
-const filterNum = num.filter(num => num > 20)
-// console.log(filterNum)
-
-/* Remove Duplicates */
-const uniqueArr = (arr) => {
-  return [...new Set(arr)]
-};
-
-const arr = [10, 3, 26, 7, 9, 3]
-const newArr = uniqueArr(arr)
-// console.log('new', newArr)
-const index = arr.findIndex((item) => item > 20)
-// console.log(index)
-if (index >= 0) { //found in arr
-  const temp = arr[index] += 1
-  // console.log(temp)
-}
-
-const array = [1, 3, 5, 7, 9]
-const fields = array.slice(0, 3)
-const newFields = array.slice(3, array.length).concat(fields)
-// console.log('newFields', newFields)
-
-/** Callback Functions 
-  Accepts functions as arguments & invoke/call it in outer function
-  Run async operations as code runs sequentially in javascript
-**/
-// setTimeout(() => { 
-//   console.log('Message is shown after 3 seconds')
-// }, 3000);
-
+/* Method */
 const circle = {
   radius: 20,
   diameter() {
-    return this.radius*2
+    return this.radius * 2
   },
   perimeter: function() { //note: return NaN for arrow function
-    return 2*Math.PI*this.radius
+    return 2 * Math.P * this.radius
   }
 }
 // console.log(circle.diameter()) //40
